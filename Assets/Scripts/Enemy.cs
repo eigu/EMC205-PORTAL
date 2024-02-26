4dad1ff6beedd6cb0ce05192fcbf0473e05ff17d
eigu <jmpheen@gmail.com>
2024-02-26 22:54:11 +0800

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+  void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.CompareTag("Object"))
+    {
+      Destroy(gameObject);
+    }
+  }
+}
